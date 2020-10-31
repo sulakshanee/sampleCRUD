a824686bbaec64a5dcb2adce25470fb94b799abf
sulakshanee <sulakshanee111@gmail.com>
2020-10-31 22:20:58 +0530

    I removed

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php  
-
-	$connection = mysqli_connect('localhost', 'root', '', 'sampledb');
-
-	//Checking the connection
-	if (mysqli_connect_errno()) {
-		die('Database connection failed' . mysqli_connect_errno());
-	}
-?>
